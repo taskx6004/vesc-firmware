426b62e9f55afe6948a3ef353b067d2604b1cc06
taskx6004 <taskx6004@gmail.com>
2019-02-18 01:56:33 +0800

    Update conf_general.c

--- conf_general.c
+++ conf_general.c
@@ -256,19 +256,16 @@ void conf_general_get_default_mc_configuration(mc_configuration *conf) {
 	conf->foc_sat_comp = MCCONF_FOC_SAT_COMP;
 	conf->foc_temp_comp = MCCONF_FOC_TEMP_COMP;
 	conf->foc_temp_comp_base_temp = MCCONF_FOC_TEMP_COMP_BASE_TEMP;
-	conf->foc_current_filter_const = MCCONF_FOC_CURRENT_FILTER_CONST;
 
 	conf->s_pid_kp = MCCONF_S_PID_KP;
 	conf->s_pid_ki = MCCONF_S_PID_KI;
 	conf->s_pid_kd = MCCONF_S_PID_KD;
-	conf->s_pid_kd_filter = MCCONF_S_PID_KD_FILTER;
 	conf->s_pid_min_erpm = MCCONF_S_PID_MIN_RPM;
 	conf->s_pid_allow_braking = MCCONF_S_PID_ALLOW_BRAKING;
 
 	conf->p_pid_kp = MCCONF_P_PID_KP;
 	conf->p_pid_ki = MCCONF_P_PID_KI;
 	conf->p_pid_kd = MCCONF_P_PID_KD;
-	conf->p_pid_kd_filter = MCCONF_P_PID_KD_FILTER;
 	conf->p_pid_ang_div = MCCONF_P_PID_ANG_DIV;
 
 	conf->cc_startup_boost_duty = MCCONF_CC_STARTUP_BOOST_DUTY;
@@ -288,7 +285,6 @@ void conf_general_get_default_mc_configuration(mc_configuration *conf) {
 	conf->m_bldc_f_sw_max = MCCONF_M_BLDC_F_SW_MAX;
 	conf->m_dc_f_sw = MCCONF_M_DC_F_SW;
 	conf->m_ntc_motor_beta = MCCONF_M_NTC_MOTOR_BETA;
-	conf->m_out_aux_mode = MCCONF_M_OUT_AUX_MODE;
 }
 
 /**
